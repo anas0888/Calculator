ee2646e3462dc5af5cb02818be0a2711e9214260
Muhammad Anas <daggaanas0888@gmail.com>
2026-06-02 16:40:33 +0530

    add functions for operations

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+const add = (a,b) => {
+return a + b
+};
+
+const sub = (a,b) => {
+return a - b;
+};
+
+const division = (a,b) => {
+return a / b
+};
+
+const multiply = (a,b) => {
+return a * b
+};
+
+let num,nextNum, operation;
+
+
+let operate = document.querySelectorAll('operations')
+
+operate = (num1,num2,operation) => {
+if(operation == '+'){
+return add(num1,num2)
+}
+else if(operation == '-'){
+return sub(num1,num2)
+}
+else if(operation == '*'){
+return multiply(num1,num2)
+}
+else if(operation == '/'){
+return division(num1,num2)
+}
+};
+
+
+
 let allBtn = document.querySelectorAll(".btn")	
 
 allBtn.forEach((Btn,index)=> {
